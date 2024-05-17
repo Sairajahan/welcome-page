1a710a8e103627b7319c6972ce2eb0142e50f5b0
Sairajahan <saira.jahan281999@gmail.com>
2024-05-17 22:48:43 +0600

    Added welcome.blade.php

---
+++ welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome Page</title>
+</head>
+<body>
+    <h1>Welcome to Our Website</h1>
+    <p>This is the welcome page. Feel free to explore our site!</p>
+</body>
+</html>
